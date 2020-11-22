ccdefc8fa265c826eadaf9979eee8cdaa374980a
Carlos Luna <carlos.lun.salazar@gmail.com>
2020-11-22 01:42:07 -0300

    primer commit entorno casi termiando

---
+++ src/components/HelloWord.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+const HelloWord = () => {
+    return (
+        <div>
+            <h1>Hola mundo</h1>
+        </div>
+    )
+} 
+
+export default HelloWord
