33a6d8907e10d6e3877f8a9df0532e0fa498a416
Carlos Luna <carlos.lun.salazar@gmail.com>
2020-12-03 20:07:47 -0300

    trabajando redux

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -4,7 +4,7 @@ import '../assets/styles/components/CarouselItem.scss'
 import playIcon from '../assets/static/play-icon.png'
 import plusIcon from '../assets/static/plus-icon.png'
 
-const CarouselItem = ({cover, title, year, contentRating, duration}) => {
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => {
     return (
         <div className="carousel-item">
             <img className="carousel-item__img" src={cover} alt="" />
@@ -13,9 +13,9 @@ const CarouselItem = ({cover, title, year, contentRating, duration}) => {
                     <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
                     <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" />
                 </div>
-    <p className="carousel-item__details--title">{title}</p>
+                <p className="carousel-item__details--title">{title}</p>
                 <p className="carousel-item__details--subtitle">
-                {`${year} ${contentRating} ${duration}`}
+                    {`${year} ${contentRating} ${duration}`}
                 </p>
             </div>
         </div>
